7c0a89a589d2f866a893c1c3b11e49b621def511
Blue Nguyen <blue.nguyen@siliconstack.com.au>
2025-07-26 12:01:52 +0700

    setup antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import "./styles/App.scss";
-import { Button } from "antd";
+import { Button, ConfigProvider } from "antd";
 
 function App() {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: '"Poppins", sans-serif',
+        },
+      }}
+    >
       <span className="container text-3xl text-red-500">Cathay shop</span>
       <Button type="primary">Click me</Button>
-    </>
+    </ConfigProvider>
   );
 }
 
