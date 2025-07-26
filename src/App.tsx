78ceb4615a65f83d00af2cc42742ed464c131963
Danh Chi Bao <cathay.danh@siliconstack.com.au>
2025-07-26 11:41:10 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./styles/App.scss";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <>
+      <span className="container text-3xl text-red-500">Cathay shop</span>
+      <Button type="primary">Click me</Button>
+    </>
+  );
+}
+
+export default App;
