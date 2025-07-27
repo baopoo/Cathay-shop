f06804aeae45c7d5bdae5f65fe36dccfeab354f6
Danh Chi Bao <cathay.danh@siliconstack.com.au>
2025-07-27 16:23:34 +0700

    Add detail page.

--- src/client/views/ProductDetail/ProductDetail.tsx
+++ src/client/views/ProductDetail/ProductDetail.tsx
@@ -6,6 +6,12 @@ const ProductDetail = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <Row gutter={[32, 32]}>
+        <Col span={24}>
+          <div className="text-[12px] text-gray-3 mb-6">
+            Home &gt; <span className="text-gray-3">Men &gt; </span>
+            <span className="text-gray-1 font-medium">Lightweight Jacket</span>
+          </div>
+        </Col>
         <Col xs={24} md={12}>
           <ProductImage />
         </Col>
